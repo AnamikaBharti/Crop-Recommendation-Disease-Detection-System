f3223ea966d7dfb39e57a42c277232fe46d4bb4b
Siddharth <gautamsiddharth131004@gmail.com>
2025-10-04 12:04:03 +0530

    Add Crop-Recommendation-Disease-Detection-System folder

---
+++ Crop-Recommendation-Disease-Detection-System/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold text-blue-400 underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
